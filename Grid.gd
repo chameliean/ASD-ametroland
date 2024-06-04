537a2c43affe318c4649c00ab754277203c6651f
Amelie <65472099+chameliean@users.noreply.github.com>
2024-06-04 12:22:31 -0400

    yippeeee

--- Grid.gd
+++ Grid.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
+# Signal to notify when the cell is clicked
+signal cell_clicked
 
-# Called when the node enters the scene tree for the first time.
+# Ready function to set up the cell
 func _ready():
-	pass # Replace with function body.
+	# Connect the input event signal to a custom function
+	connect("input_event", self, "_on_input_event")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Function to handle input events
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_LEFT:
+			# Emit the cell_clicked signal when the cell is clicked
+			emit_signal("cell_clicked")
+			# Add any additional logic for when the cell is clicked
+			print("Cell clicked at position: ", position)
